f79f4259e4923da288619fec3383d950175bf886
wangyuting322 <2428282291@qq.com>
2021-09-24 17:15:47 +0800

    feat -> 优化代码

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -32,11 +32,13 @@ export default {
       timer: null,
       mixer: null,
       AnimationAction: null,
-    //   stats: null,
+      stats: null,
       planeInfo: null,
       animationTimer: null,
       raycaster: null,
-      mouse: null
+      mouse: null,
+      isNeedStats: false,
+      isNeedProperty: false
     };
   },
   methods: {
@@ -49,25 +51,25 @@ export default {
       document
         .querySelector(".canvas-frame")
         .appendChild(this.stats.domElement);
-      // document.body.appendChild(this.stats.domElement)
     },
     /**
      * 初始化场景
      */
     initScene() {
       this.scene = new THREE.Scene();
+      //   this.scene.autoUpdate = true
     },
     /**
      * 初始化灯光
      */
     initLight() {
       // 平行光，模拟远处类似太阳的光源
-      const directionalLight = track(new THREE.DirectionalLight(0xffffff, 0.3));
+      const directionalLight = new THREE.DirectionalLight(0xffffff, 0.3);
       directionalLight.color.setHSL(0.1, 1, 0.95);
       directionalLight.position.set(0, 200, 0).normalize();
       this.scene.add(directionalLight);
       // 环境光AmbientLight,影响整个场景的光源
-      const ambient = track(new THREE.AmbientLight(0xffffff, 1));
+      const ambient = new THREE.AmbientLight(0xffffff, 1);
       ambient.position.set(0, 0, 0);
       this.scene.add(ambient);
     },
@@ -82,6 +84,7 @@ export default {
         10000
       );
       this.camera.position.set(-250, 0, 0);
+      this.camera.lookAt(this.scene.position);
     },
     /**
      * 初始化渲染器
@@ -113,20 +116,6 @@ export default {
       this.controls.maxDistance = 5000;
       // 上下翻转的最大角度
       this.controls.maxPolarAngle = 1.5;
-//       this.controls.mouseButtons = {
-// 	LEFT: THREE.MOUSE.ROTATE,
-// 	MIDDLE: THREE.MOUSE.DOLLY,
-// 	// RIGHT: THREE.MOUSE.RIGHT
-// 	RIGHT: null
-// }
-// this.controls.enablePan=true;// 允许平移 只有这个设为true时，下面两个参数才有意义
-
-// this.controls.hylMovePanY=false;//关闭垂直平移相机
-
-// this.controls.hylMovePanX=true;// 开启水平移动相机
-
-      // this.controls.maxPolarAngle = Math.PI / 2
-      //   this.controls.target = new THREE.Vector3(50, 50, 0);
     },
     /**
      * 动画
@@ -135,13 +124,13 @@ export default {
       cancelAnimationFrame(this.animationTimer);
       this.controls.update();
       this.renderer.render(this.scene, this.camera);
-    //   this.stats.update();
+      //   this.stats.update();
       this.animationTimer = requestAnimationFrame(this.animate);
     },
     /**
      * 添加背景
-     * @param {*} size 盒子大小  */
-    addSkybox(size) {
+     */
+    addSkybox() {
       const urls = [
         require("@/assets/img/skybox/远山_RT.jpg"), // right
         require("@/assets/img/skybox/远山_LF.jpg"), // left
@@ -150,48 +139,74 @@ export default {
         require("@/assets/img/skybox/远山_BK.jpg"), // back
         require("@/assets/img/skybox/远山_FR.jpg") // front
       ];
-      track(new THREE.CubeTextureLoader()).load(urls, oj => {
+      new THREE.CubeTextureLoader().load(urls, oj => {
         this.scene.background = oj;
       });
+    },
+    /**
+     * 创建精灵模型
+     */
+    createSprite() {
+      var canvas = document.createElement("canvas");
+      canvas.width = 512;
+      canvas.height = 180;
+      var c = canvas.getContext("2d");
+      // 矩形区域填充背景
+      c.fillStyle = "#ffffff";
+      c.fillRect(0, 0, 512, 180);
+      c.beginPath();
+      // 文字
+      c.beginPath();
+      c.translate(250, 20);
+      c.fillStyle = "#000000"; //文本填充颜色
+      c.font = "bold 48px 宋体"; //字体样式设置
+      c.textBaseline = "middle"; //文本与fillText定义的纵坐标
+      c.textAlign = "center"; //文本居中(以fillText定义的横坐标)
+      c.fillText(`航班号： CA1234`, 0, 10);
+      c.fillText(`计划时间： 10:45`, 0, 65);
+      c.fillText(`目的地： 重庆`, 0, 120);
+
+      document.body.appendChild(canvas);
+      // canvas画布可以理解为一张图片
+      var texture = new THREE.CanvasTexture(canvas);
+      document.body.removeChild(canvas);
+      // 创建精灵材质对象SpriteMaterial
+      var spriteMaterial = new THREE.SpriteMaterial({
+        map: texture, //设置精灵纹理贴图
+        transparent: true //开启透明(纹理图片png有透明信息)
+      });
+      // 创建精灵模型对象，不需要几何体geometry参数
+      var sprite = new THREE.Sprite(spriteMaterial);
+      return sprite;
     },
     /**
      * 创建网格模型
      */
     createMesh() {
-      this.clock = track(new THREE.Clock());
+      this.clock = new THREE.Clock();
       // 加载模型路径
       const MODEL_PATH = this.objUrl;
       // 加载模型
-      const loader = track(new OBJLoader());
+      const loader = new OBJLoader();
       loader.load(
         MODEL_PATH,
         loadedMesh => {
           // 加载图片贴图
-          const loader1 = track(new THREE.TextureLoader());
+          const loader1 = new THREE.TextureLoader();
           loader1.load(require("@/assets/img/airplane_body.jpg"), texture => {
-            const bodymaterial = track(
-              new THREE.MeshPhongMaterial({
-                map: texture,
-                color: 0xe3e4df
-              })
-            );
-            // 另一种方法，分开写
-            bodymaterial.map.matrix.scale(1, 1); // 缩放
-            // material.transparent = true
-            bodymaterial.opacity = 1;
-            const othermaterial = track(
-              new THREE.MeshPhongMaterial({
-                color: 0xe3e4df
-              })
-            );
-            const wheelmaterial = track(
-              new THREE.MeshPhongMaterial({
-                color: 0x000000
-              })
-            );
+            const bodymaterial = new THREE.MeshPhongMaterial({
+              map: texture,
+              color: 0xe3e4df
+            });
+            const othermaterial = new THREE.MeshPhongMaterial({
+              color: 0xe3e4df
+            });
+
+            const wheelmaterial = new THREE.MeshPhongMaterial({
+              color: 0x000000
+            });
             // 加载完obj文件是一个场景组，遍历它的子元素，赋值纹理并且更新面和点的发现了
             loadedMesh.children.forEach(child => {
-              // console.log(child.name)
               if (
                 [
                   "11803_Airplane_body",
@@ -214,85 +229,34 @@ export default {
               }
             });
             // themodel为加载的模型实例
-            // console.log(loadedMesh)
             this.theModel1 = loadedMesh;
             // 模型位置改变
             this.theModel1.position.set(0, 0, 0);
             // 模型大小改变
             const smallNnm = 0.05;
             this.theModel1.scale.set(smallNnm, smallNnm, smallNnm);
-            this.theModel1.rotation.set(-Math.PI / 2, 0, 0);
-
-            // 添加圆形模型
-            // let tipCircle = new THREE.CircleGeometry(guiControls.radius, guiControls.segments, guiControls.thetaStart, guiControls.thetaLength)
+            this.theModel1.rotateX(-Math.PI / 2);
 
             /**
              * 创建点精灵模型
              */
-            var canvas = document.createElement("canvas");
-            canvas.width = 512;
-            canvas.height = 180;
-            var c = canvas.getContext("2d");
-            // 矩形区域填充背景
-            c.fillStyle = "#ffffff";
-            c.fillRect(0, 0, 512, 180);
-            c.beginPath();
-            // 文字
-            c.beginPath();
-            c.translate(250, 20);
-            c.fillStyle = "#000000"; //文本填充颜色
-            c.font = "bold 48px 宋体"; //字体样式设置
-            c.textBaseline = "middle"; //文本与fillText定义的纵坐标
-            c.textAlign = "center"; //文本居中(以fillText定义的横坐标)
-            c.fillText(`航班号： CA1234`, 0, 10);
-            c.fillText(`计划时间： 10:45`, 0, 65);
-            c.fillText(`目的地： 重庆`, 0, 120);
-
-            document.body.appendChild(canvas);
-            // canvas画布可以理解为一张图片
-            var texture = track(new THREE.CanvasTexture(canvas));
-            document.body.removeChild(canvas)
-            // 创建精灵材质对象SpriteMaterial
-            var spriteMaterial = track(
-              new THREE.SpriteMaterial({
-                map: texture, //设置精灵纹理贴图
-                transparent: true //开启透明(纹理图片png有透明信息)
-              })
-            );
-            // 创建精灵模型对象，不需要几何体geometry参数
-            var sprite = track(new THREE.Sprite(spriteMaterial));
-            sprite.position.set(20, 20, 20);
-            sprite.scale.set(50, 30, 10); //精灵大小
+            let sprite = this.createSprite();
             // 把精灵模型插入到模型对象的父对象下面
             this.theModel1.add(sprite);
-            // 父对象group位置变化,网格模型及其对象的标签同样发生变化
-            // group.position.set(10, 0, -80);
-            // 表示标签信息的精灵模型对象相对父对象设置一定的偏移
-            // sprite.translateY(30);
-            // this.scene.add(sprite)
-            // console.log(this.theModel1);
-            // sprite.position.set(20, 20, 20);
-            // sprite.scale.set(50, 30, 10); //精灵大小
             this.scene.add(this.theModel1);
-            this.theModel1.children[
-              this.theModel1.children.length - 1
-            ].translateZ(1000);
-            this.theModel1.children[
-              this.theModel1.children.length - 1
-            ].scale.set(1000, 600, 200);
-            this.theModel1.children.map(item => {
+            this.theModel1.traverse(item => {
+              // 修改精灵模型大小
+              if (item instanceof THREE.Sprite) {
+                item.translateZ(1000);
+                item.scale.set(1000, 600, 200);
+              }
+              //   添加航班信息
               item.info = {
                 name: "CA1234",
                 time: "10:45",
                 aim: "重庆"
               };
-              return item;
             });
-            this.theModel1.info = {
-              name: "CA1234",
-              time: "10:45",
-              aim: "重庆"
-            };
           });
         },
         undefined,
@@ -305,40 +269,32 @@ export default {
     创建地板/跑道
     */
     createFloor() {
-      const loader = track(new THREE.TextureLoader());
+      const loader = new THREE.TextureLoader();
       loader.load(require("@/assets/img//skybox/远山_DN.jpg"), texture => {
         // wrapS是x轴方向的行为，wrapT是y轴方向的行为
         // texture.wrapS = texture.wrapT = THREE.RepeatWrapping// THREE.RepeatWrapping:允许重复自己
         // texture.repeat.set(10, 10)
-        const floorGeometry = track(new THREE.BoxGeometry(2000, 5, 2000));
-        const floorMaterial = track(
-          new THREE.MeshBasicMaterial({
-            map: texture
-          })
-        );
-        const floor = track(new THREE.Mesh(floorGeometry, floorMaterial));
+        const floorGeometry = new THREE.BoxGeometry(2000, 5, 2000);
+        const floorMaterial = new THREE.MeshBasicMaterial({
+          map: texture
+        });
+        const floor = new THREE.Mesh(floorGeometry, floorMaterial);
         floor.name = "地面草地";
         floor.scale.set(10, 1, 20);
-        // floor.rotateY(-Math.PI / 2)
         floor.position.set(0, -15, 0);
         this.scene.add(floor);
       });
       loader.load(require("@/assets/img/跑道.png"), texture => {
-        // wrapS是x轴方向的行为，wrapT是y轴方向的行为
-        // texture.wrapS = texture.wrapT = THREE.RepeatWrapping// THREE.RepeatWrapping:允许重复自己
-        // texture.repeat.set(10, 10)
-        const floorGeometry = track(new THREE.BoxGeometry(30, 5, 100));
-        const floorMaterial = track(
-          new THREE.MeshBasicMaterial({
-            map: texture
-          })
-        );
-        const floor = track(new THREE.Mesh(floorGeometry, floorMaterial));
-        floor.name = "地面";
-        floor.scale.set(10, 1, 20);
-        floor.rotateY(-Math.PI / 2);
-        floor.position.set(850, -10, 0);
-        this.scene.add(floor);
+        const roadGeometry = new THREE.BoxGeometry(30, 5, 100);
+        const roadMaterial = new THREE.MeshBasicMaterial({
+          map: texture
+        });
+        const road = new THREE.Mesh(roadGeometry, roadMaterial);
+        road.name = "跑道";
+        road.scale.set(10, 1, 20);
+        road.rotateY(-Math.PI / 2);
+        road.position.set(850, -10, 0);
+        this.scene.add(road);
       });
     },
     /**
@@ -349,26 +305,28 @@ export default {
       /**
        * 创建一个设置重复纹理的管道
        */
-      this.curve = track(
-        new THREE.CatmullRomCurve3(
-          [
-            new THREE.Vector3(0, 0, 0),
-            new THREE.Vector3(500, 100, 0),
-            new THREE.Vector3(1000, 400, 0),
-            new THREE.Vector3(2000, 470, 0),
-            new THREE.Vector3(3000, 400, 0),
-            new THREE.Vector3(3500, 100, 0),
-            new THREE.Vector3(4000, 0, 0)
-          ],
-          false /* 是否闭合 */
-          // 'centripetal',
-          // 0.9
-        )
+      this.curve = new THREE.CatmullRomCurve3(
+        [
+          new THREE.Vector3(0, 0, 0),
+          new THREE.Vector3(500, 100, 0),
+          new THREE.Vector3(1000, 400, 0),
+          new THREE.Vector3(2000, 470, 0),
+          new THREE.Vector3(3000, 400, 0),
+          new THREE.Vector3(3500, 100, 0),
+          new THREE.Vector3(4000, 0, 0)
+        ],
+        false /* 是否闭合 */
+        // 'centripetal',
+        // 0.9
       );
-      let tubeGeometry = track(
-        new THREE.TubeGeometry(this.curve, 100, 0.6, 30, false)
+      let tubeGeometry = new THREE.TubeGeometry(
+        this.curve,
+        100,
+        0.6,
+        30,
+        false
       );
-      var textureLoader = track(new THREE.TextureLoader());
+      var textureLoader = new THREE.TextureLoader();
       const texture = textureLoader.load(require("@/assets/img/logo.png"));
       // 设置阵列模式为 RepeatWrapping
       texture.wrapS = THREE.RepeatWrapping;
@@ -376,14 +334,12 @@ export default {
       // 设置x方向的偏移(沿着管道路径方向)，y方向默认1
       // 等价texture.repeat= new THREE.Vector2(20,1)
       texture.repeat.x = 20;
-      var tubeMaterial = track(
-        new THREE.MeshPhongMaterial({
-          map: texture,
-          transparent: true
-        })
-      );
+      var tubeMaterial = new THREE.MeshPhongMaterial({
+        map: texture,
+        transparent: true
+      });
       this.texture = texture;
-      var tube = track(new THREE.Mesh(tubeGeometry, tubeMaterial));
+      var tube = new THREE.Mesh(tubeGeometry, tubeMaterial);
       this.scene.add(tube);
       /**
        * 创建一个半透明管道
@@ -399,6 +355,9 @@ export default {
 
       //   this.scene.add(new THREE.AxesHelper(300))
     },
+    /**
+     * 鼠标点击 - 创建镭射
+     */
     initMouse() {
       this.raycaster = new THREE.Raycaster();
       this.mouse = new THREE.Vector2();
@@ -407,14 +366,14 @@ export default {
      * 全部初始化
      */
     initAll() {
-    //   this.initStats();
+      this.isNeedStats && this.initStats();
       this.initScene();
       this.initLight();
       this.addSkybox();
       this.createFloor();
       this.createMesh();
       this.createCube();
-    //   this.initMouse();
+      this.isNeedProperty && this.initMouse();
       this.initCamera();
       this.initRenderer();
       this.initControls();
@@ -433,8 +392,11 @@ export default {
       this.camera.updateProjectionMatrix();
       // 修改渲染的画布的大小
       this.renderer.setSize(containerWidth, containerHeight);
-      this.animate();
+      this.renderer.this.animate();
     },
+    /**
+     * 开始运动
+     */
     startMove() {
       this.progress = 0;
       if (this.timer) clearInterval(this.timer);
@@ -442,8 +404,8 @@ export default {
         if (this.progress > 1.0) {
           return; // 停留在管道末端,否则会一直跑到起点 循环再跑
         }
-        if (this.curve&&this.theModel1) {
-            this.progress += 0.0009;
+        if (this.curve && this.theModel1) {
+          this.progress += 0.0009;
           const point = this.curve.getPoint(this.progress);
           if (point && point.x) {
             this.theModel1.position.set(point.x, point.y, point.z);
@@ -463,7 +425,6 @@ export default {
       // 获取container元素的左边距和上边距
       let leftDistance = getOffsetLeft(this.container, true);
       let topDistance = getOffsetTop(this.container, true);
-      console.log(leftDistance, topDistance);
       // 获取相对于浏览器位置的鼠标坐标
       let x, y;
       if (event.changedTouches) {
@@ -489,32 +450,25 @@ export default {
       this.planeInfo = firstObj.info ? firstObj.info : null;
       this.$forceUpdate();
     },
+    /**
+     * 清空内存
+     */
     disposeAll() {
       try {
-        this.theModel1.traverse(item => {
-          if (item instanceof THREE.Mesh) {
-            item.geometry.dispose(); // 删除几何体
-            item.material.dispose(); // 删除材质
-          }else if(item instanceof THREE.Sprite){
-            console.log(item.geometry.dispose,item.material.dispose);
-          }
+        this.scene.traverse(item => {
+          track(item);
         });
+        resMgr && resMgr.dispose();
+        THREE.Cache.clear();
+        this.scene = null;
         this.raycaster = null;
         this.mouse = null;
-        console.log(this.scene.dispose);
-        this.scene.dispose();
-        resMgr && resMgr.dispose();
+        this.renderer.clear();
         this.renderer.dispose();
         this.renderer.forceContextLoss();
-        this.renderer.content = null;
         cancelAnimationFrame(this.animationTimer);
-        this.renderer.domElement = null;
         this.renderer = null;
-        // let gl = renderer.domElement.getContext("webgl");
-        // gl && gl.getExtension("WEBGL_lose_context").loseContext();
-        // console.log(renderer.info)   //查看memery字段即可
-        console.log('ok');
-        
+        console.log("ok");
       } catch (e) {
         console.log(e);
       }
@@ -530,8 +484,10 @@ export default {
     // 监听窗口的大小改变
     window.onresize = this.onWindowResize;
     // 仅监听3D部分的模型点击事件
-    // this.container.addEventListener("click", this.clickModel, false);
-    window.addEventListener("beforeunload", (e)=> {
+    if (this.isNeedProperty) {
+      this.container.addEventListener("click", this.clickModel, false);
+    }
+    window.addEventListener("beforeunload", e => {
       this.disposeAll();
     });
   },
@@ -565,7 +521,7 @@ export default {
         <div class="container"></div>
         <div class="canvas-frame"></div>
         {/** 属性展示面板内容 */}
-        {false ? (
+        {this.isNeedProperty ? (
           <div class="threed-property-content">
             {this.planeInfo ? (
               <div>
@@ -588,7 +544,7 @@ export default {
           </div>
         ) : null}
         <button class="btn" onClick={this.startMove}>
-          开始运动
+          从头开始运动
         </button>
       </div>
     );
@@ -619,11 +575,15 @@ export default {
   display: flex;
   justify-content: space-between;
 }
-.btn {
+.btn,
+.btn2 {
   position: fixed;
   padding: 10px 15px;
   font-size: 20px;
   left: 10px;
   bottom: 10px;
+}
+.btn2 {
+  left: 30px;
 }
 </style>
--- src/utils/common.js
+++ src/utils/common.js
@@ -1,19 +1,19 @@
-import * as THREE from 'three/build/three.module'
+import * as THREE from "three/build/three.module";
 /**
  * 获取元素节点的左边距
  * @param {Object} elementObj 元素节点
  * @param {Bealoon} isRoot 是否获取当前元素相对于浏览器（html节点）的左边距
  */
-export function getOffsetLeft (elementObj, isRoot = true) {
-  let tmp = elementObj.offsetLeft
+export function getOffsetLeft(elementObj, isRoot = true) {
+  let tmp = elementObj.offsetLeft;
   if (isRoot) {
-    let val = elementObj.offsetParent
+    let val = elementObj.offsetParent;
     while (val != null) {
-      tmp += val.offsetLeft
-      val = val.offsetParent
+      tmp += val.offsetLeft;
+      val = val.offsetParent;
     }
   }
-  return tmp
+  return tmp;
 }
 
 /**
@@ -21,16 +21,16 @@ export function getOffsetLeft (elementObj, isRoot = true) {
  * @param {Object} elementObj 元素节点
  *  * @param {Bealoon} isRoot 是否获取当前元素相对于浏览器（html节点）的上边距
  */
-export function getOffsetTop (elementObj, isRoot = true) {
-  let tmp = elementObj.offsetTop
+export function getOffsetTop(elementObj, isRoot = true) {
+  let tmp = elementObj.offsetTop;
   if (isRoot) {
-    let val = elementObj.offsetParent
+    let val = elementObj.offsetParent;
     while (val != null) {
-      tmp += val.offsetTop
-      val = val.offsetParent
+      tmp += val.offsetTop;
+      val = val.offsetParent;
     }
   }
-  return tmp
+  return tmp;
 }
 
 /**
@@ -41,84 +41,85 @@ export function getOffsetTop (elementObj, isRoot = true) {
  * @param {Array}          exclude 严格模式下, 略过校验的字段
  * ---
  */
-export function isEmpty (data, strict, exclude = []) {
-  if (Object.prototype.toString.call(data) === '[object Array]') {
-    return data.length === 0
+export function isEmpty(data, strict, exclude = []) {
+  if (Object.prototype.toString.call(data) === "[object Array]") {
+    return data.length === 0;
   }
-  if (Object.prototype.toString.call(data) === '[object Object]') {
+  if (Object.prototype.toString.call(data) === "[object Object]") {
     if (Object.values(data).length === 0) {
-      return true
+      return true;
     }
     if (strict) {
-      return Object.keys(data).findIndex((field) => (
-        exclude.includes(field)
-          ? false
-          : !this.isEmpty(data[field]))) === -1
+      return (
+        Object.keys(data).findIndex(field =>
+          exclude.includes(field) ? false : !this.isEmpty(data[field])
+        ) === -1
+      );
     }
-    return false
+    return false;
   }
-  return (data === null || data === undefined) ? true : /^[\s\xa0]*$/.test(data)
+  return data === null || data === undefined ? true : /^[\s\xa0]*$/.test(data);
 }
-export  class ResourceTracker {
-   constructor() {
-   	this.resources = new Set();
-   }
-   track(resource) {
-   	if (!resource) {
-   		return resource;
-   	}
+export class ResourceTracker {
+  constructor() {
+    this.resources = new Set();
+  }
+  track(resource) {
+    if (!resource) {
+      return resource;
+    }
+    // handle children and when material is an array of materials or
+    // uniform is array of textures
+    if (Array.isArray(resource)) {
+      resource.forEach(resource => this.track(resource));
+      return resource;
+    }
 
-   	// handle children and when material is an array of materials or
-   	// uniform is array of textures
-   	if (Array.isArray(resource)) {
-   		resource.forEach(resource => this.track(resource));
-   		return resource;
-   	}
-
-   	if (resource.dispose || resource instanceof THREE.Object3D) {
-   		this.resources.add(resource);
-   	}
-   	if (resource instanceof THREE.Object3D) {
-   		this.track(resource.geometry);
-   		this.track(resource.material);
-   		this.track(resource.children);
-   	} else if (resource instanceof THREE.Material) {
-   		// We have to check if there are any textures on the material
-   		for (const value of Object.values(resource)) {
-   			if (value instanceof THREE.Texture) {
-   				this.track(value);
-   			}
-   		}
-   		// We also have to check if any uniforms reference textures or arrays of textures
-   		if (resource.uniforms) {
-   			for (const value of Object.values(resource.uniforms)) {
-   				if (value) {
-   					const uniformValue = value.value;
-   					if (uniformValue instanceof THREE.Texture ||
-   						Array.isArray(uniformValue)) {
-   						this.track(uniformValue);
-   					}
-   				}
-   			}
-   		}
-   	}
-   	return resource;
-   }
-   untrack(resource) {
-   	this.resources.delete(resource);
-   }
-   dispose() {
-   	for (const resource of this.resources) {
-   		if (resource instanceof THREE.Object3D) {
-   			if (resource.parent) {
-   				resource.parent.remove(resource);
-   			}
-   		}
-   		if (resource.dispose) {
-   			resource.dispose();
-   		}
-   	}
-   	this.resources.clear();
-   }
+    if (resource.dispose || resource instanceof THREE.Object3D) {
+      this.resources.add(resource);
+    }
+    if (resource instanceof THREE.Object3D) {
+      this.track(resource.geometry);
+      this.track(resource.material);
+      this.track(resource.children);
+    } else if (resource instanceof THREE.Material) {
+      // We have to check if there are any textures on the material
+      for (const value of Object.values(resource)) {
+        if (value instanceof THREE.Texture) {
+          this.track(value);
+        }
+      }
+      // We also have to check if any uniforms reference textures or arrays of textures
+      if (resource.uniforms) {
+        for (const value of Object.values(resource.uniforms)) {
+          if (value) {
+            const uniformValue = value.value;
+            if (
+              uniformValue instanceof THREE.Texture ||
+              Array.isArray(uniformValue)
+            ) {
+              this.track(uniformValue);
+            }
+          }
+        }
+      }
+    }
+    return resource;
+  }
+  untrack(resource) {
+    this.resources.delete(resource);
+  }
+  dispose() {
+    this.resources.forEach(resource => {
+      if (resource instanceof THREE.Object3D) {
+        if (resource.parent) {
+          resource.parent.remove(resource);
+        }
+      }
+      if (resource.dispose) {
+        resource.dispose();
+      }
+    });
+    this.resources.clear();
+  }
 }
-
